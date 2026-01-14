db9a3c92a2be6b27786ee8445f4f166af632e982
simuneu <worksimu@gmail.com>
2026-01-15 00:03:07 +0900

    fix:xml문법 오류 수정(최근 커밋 <>)

--- src/main/java/com/chrono/ChronoApplication.java
+++ src/main/java/com/chrono/ChronoApplication.java
@@ -1,9 +1,11 @@
 package com.chrono;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.chrono.mapper")
 public class ChronoApplication {
 
 	public static void main(String[] args) {
